38d7a06c4b1f0badd678494b2845aad395482827
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2024-12-19 23:55:33 -0300

    3_CORE_MODULES/9 added

---
+++ 3_CORE_MODULES/9_renomeando_arquivo/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+let arquivoAntigo = 'arquivo.txt'
+let arquivoNovo = 'novo.txt'
+
+fs.rename(arquivoAntigo, arquivoNovo, function(err){
+    if(err){
+        if(err.code === 'ENOENT'){
+            console.log(`${arquivoAntigo} não encontrado!`)
+            return
+        }else{
+            console.log(err)
+            return
+        }
+    }
+    console.log(`O arquivo ${arquivoAntigo}, foi renomeado como ${arquivoNovo}`)
+})

420cce006f1a8c77dc0b4414c735f2be036c8a36
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2025-02-20 23:03:29 -0300

    5_ACCOUNTS função sair adicionada

---
+++ 5_ACCOUNTS/index.js
@@ -0,0 +1,80 @@
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const fs = require('fs')
+
+
+operation()
+
+function operation(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'O que você deseja fazer?',
+            choices: [
+                'Criar Conta',
+                'Consultar Saldo',
+                'Depositar',
+                'Sacar',
+                'Sair',
+            ],
+        },
+    ]).then((answer) => {
+        const action = answer['action']
+        switch(action){
+            case "Criar Conta":
+                createAccount()
+                break
+            case "Consultar Saldo":
+                break
+            case 'Depositar':
+                break
+            case 'Sacar':
+                break
+            case 'Sair':
+                console.log(chalk.bgBlue.black('Obrigado por usar o Accounts!'))
+                process.exit()
+                break
+        }
+    })
+    .catch((err) => console.log(err))
+}
+
+function createAccount(){
+    console.log(chalk.bgGreen.black('Parabéns po escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+    buildAccount()
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para a sua conta:',
+        },
+    ])
+    .then((aswer) => {
+        const accountName = aswer['accountName']
+
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Essa contá já existe!'))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`,'{"balance": 0}',
+            function(err){
+                console.log('Erro ao criar conta ' + err)
+            },
+        )
+
+        console.log(chalk.green('Parabéns a sua conta foi criada,'))
+        operation()
+    })
+    .catch((err) => console.log(err))
+}

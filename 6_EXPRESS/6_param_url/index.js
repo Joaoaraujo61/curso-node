13a80c2fe1b9f1c4348f09bdc61aa924c02a0412
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2025-04-23 11:42:33 -0300

    6_param url added

--- 6_EXPRESS/6_param_url/index.js
+++ 6_EXPRESS/6_param_url/index.js
@@ -5,8 +5,12 @@ const port = 3000
 const path = require('path')
 const basePath = path.join(__dirname, 'templates')
 
-app.get('/', (req, res)=>{
-    res.sendFile(`${basePath}/index.html`)
+app.get('/users/:id', (req, res)=>{
+    const id = req.params.id
+
+    console.log(`Buscando user com id ${id}`)
+
+    res.sendFile(`${basePath}/users.html`)
 })
 
 app.get('/', (req, res) => {
